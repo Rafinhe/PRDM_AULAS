7ac590c03b66f3b69f77df5a446f0ce1f6d96463
Rafinhe <rafaela.mazzo.20@gmail.com>
2026-08-05 21:33:53 -0300

    Atualizacao do codigo

--- app/src/main/java/br/edu/ifsp/scl/sc3051714/contador/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3051714/contador/MainActivity.kt
@@ -1,14 +1,19 @@
 package br.edu.ifsp.scl.sc3051714.contador
 
 import android.os.Bundle
-import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import br.edu.ifsp.scl.sc3051714.contador.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val activityMainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     private lateinit var initialEt: EditText //lateinit -> nada eh nulo e eh obrigado a inicializar
     private lateinit var visorTv: TextView
@@ -20,34 +25,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         //set content faz com que o app exista!! não tem como fazer a linha do initial find antes dessa
-
         setContentView(R.layout.activity_main)  //classe R tem subclass layout e tem atributo activity_main
 
-
-        //enableEdgeToEdge() //app atravesse barra de notificacao se tiver nos cantos
 
         initialEt = findViewById(R.id.initial_et)
         visorTv = findViewById(R.id.visor_tv)
         incrementBy = findViewById(R.id.increment_bt)
 
+
         //se tem tela tem listener
         incrementBy.setOnClickListener { //sempre que ultimo parametro de uma func for outra funcao ou interface, posso tira os () do final
 
-            //new OnClickListener(){
-            //public void onClick(View v){
-
-            value++
-            visorTv.text = value.toString() //kotlin
-            //visorTv.setText(value.toString()) -> jeito em java
-
-            //ou poderia ser somente assim:
             value++.let{visorTv.text = it.toString()}
 
+            setContentView(activityMainBinding.root)
 
+            with (activityMainBinding) {
 
+                initialEt.addTextChangedListener {
+                    value = it.toString().toIntOrNull() ?: 0
+                }
 
+                incrementBt.setOnClickListener {
+                    value++.let { visorTv.text = it.toString() }
+                }
+
+            }
 
         }
 
     }
+
 }
